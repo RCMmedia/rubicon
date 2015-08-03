beebbf34d1eb68746cdecc99a230c5015549a2b1
Jason Monsef <jasonmonsef@gmail.com>
2015-08-03 14:32:15 -0700

    really?

--- wp-content/themes/rubicon/header.php
+++ wp-content/themes/rubicon/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<meta name="viewport" content="width=device-width" initial-scale="1.0">
 
 <title><?php wp_title(''); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
