5859ac7ff9de287a430aad1456eb19d4cd7d1669
Jason Monsef <jasonmonsef@gmail.com>
2015-11-30 11:50:41 -0800

    added rubicon new location

--- wp-content/themes/rubicon/page_templates/page-coming-soon.php
+++ wp-content/themes/rubicon/page_templates/page-coming-soon.php
@@ -13,10 +13,13 @@ get_header(); ?>
 				<img src="<?php bloginfo('template_url') ?>/images/homepage/border-bottom.png" alt="border" />
 			</div><!-- .border -->
 			<div class="content clearfix">
+				<div class="location-render">
+					<img class="responsive" src="<?php bloginfo('template_url') ?>/images/location/la-jolla-rendering.jpg" alt="la-jolla-rendering" width="1156" height="473" />
+				</div><!-- .left-location -->
+				<div class="location-info">
+					<img class="responsive" src="http://maps.google.com/maps/api/staticmap?center=32.871598,-117.217494&zoom=15&markers=32.872203,-117.217580|32.872203,-117.217580&path=color:0x0000FF80|weight:5|32.87229,-117.21708&size=1020x300&sensor=true"/>
+				</div><!-- .right-map -->
 				
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-					<?php the_content() ?>
-				<?php endwhile; ?>
 				
 			</div><!-- .content -->
 		</div><!-- .inner -->
