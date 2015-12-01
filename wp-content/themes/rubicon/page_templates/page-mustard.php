aa56926b7ad650a39f78ba64925b55a207fcd701
garrett cullen <info@garrettcullen.com>
2015-12-01 10:32:25 -0800

    mustard

--- wp-content/themes/rubicon/page_templates/page-mustard.php
+++ wp-content/themes/rubicon/page_templates/page-mustard.php
@@ -45,7 +45,7 @@ get_header(); ?>
 				
 				<div class="mustard_overlay">
 				
-				<span style="font-size:16px;margin-top:28px">Water, sugar, mustard seed, vinegars, honey, cranberries, fruit juice concentrates ( cherry, plumb and cranberry) white wine, salt, wheat flour, high fructose corn syrup, lemon juice, corn syrup, modified corn starch, citric acid, xanthan gum, onions, ginger, FD&C red no.40 and 3, FD&C blue no. 1 and 2, soybean and sunflour oil, spices, sodium benzoate and potassium sorbate, tumeric, garlic, annatta, calcium disodium edta.</span>
+				<span style="margin-top:20px">Water, sugar, mustard seed, vinegars, honey, cranberries, fruit juice concentrates ( cherry, plumb and cranberry) white wine, salt, wheat flour, high fructose corn syrup, lemon juice, corn syrup, modified corn starch, citric acid, xanthan gum, onions, ginger, FD&C red no.40 and 3, FD&C blue no. 1 and 2, soybean and sunflour oil, spices, sodium benzoate and potassium sorbate, tumeric, garlic, annatta, calcium disodium edta.</span>
 				</div><!-- mustard_overlay -->
 			
 			</div><!-- mustard_img_wrap -->
@@ -65,7 +65,7 @@ get_header(); ?>
 				
 				<div class="mustard_overlay">
 				
-				<span style="margin-top:20px">Water, red chili and habanero peppers, sugar, mustard seed, white distilled vinegar, honey, wheat flour, salt, garlic, xanthan gum, lemon juice, soybean oil, onions, citric acid, tumeric, paprika, calcium disodium edta, spices, annatto, natural flavor, ginger.</span>
+				<span style="font-size:16px;margin-top:28px">Water, red chili and habanero peppers, sugar, mustard seed, white distilled vinegar, honey, wheat flour, salt, garlic, xanthan gum, lemon juice, soybean oil, onions, citric acid, tumeric, paprika, calcium disodium edta, spices, annatto, natural flavor, ginger.</span>
 				</div><!-- mustard_overlay -->
 			
 			</div><!-- mustard_img_wrap -->
